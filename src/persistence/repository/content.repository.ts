67871b0de6ea27fea15ddfa456904be36c8ea1a1
Luis Gustavo <luisgustavomacedo13@gmail.com>
2024-04-25 08:26:05 -0300

    find by id content repo

--- src/persistence/repository/content.repository.ts
+++ src/persistence/repository/content.repository.ts
@@ -2,6 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ContentEntity } from '@src/core/entity/content.entity';
 import { Prisma } from '@prisma/prisma-client';
+import { MovieEntity } from '@src/core/entity/movie.entity';
+import { VideoEntity } from '@src/core/entity/video.entity';
+import { ThumbnailEntity } from '@src/core/entity/thumbnail.entity';
+
+const contentInclude = Prisma.validator<Prisma.ContentInclude>()({
+  Movie: {
+    include: { Video: true, Thumbnail: true },
+  },
+});
 
 @Injectable()
 export class ContentRepository {
@@ -47,6 +56,92 @@ export class ContentRepository {
     } catch (e) {
       this.handleAndThrowError(e);
     }
+  }
+
+  async findById(id: string): Promise<ContentEntity | undefined> {
+    try {
+      const content = await this.model.findUnique({
+        where: { id },
+        include: {
+          Movie: {
+            include: { Video: true, Thumbnail: true },
+          },
+        },
+      });
+
+      if (!content) {
+        return;
+      }
+
+      return this.mapToEntity(content);
+    } catch (e) {
+      this.handleAndThrowError(e);
+    }
+  }
+
+  private mapToEntity<
+    T extends Prisma.ContentGetPayload<{
+      include: typeof contentInclude;
+    }>,
+  >(content: T): ContentEntity {
+    if (!content.Movie) {
+      throw new Error('Content not found');
+    }
+    const contentEntity = ContentEntity.createFrom({
+      id: content.id,
+      title: content.title,
+      description: content.description,
+      type: content.type,
+      createdAt: new Date(content.createdAt),
+      updatedAt: new Date(content.updatedAt),
+    });
+
+    if (this.isMovie(content) && content.Movie.Video) {
+      const video = VideoEntity.createFrom({
+        id: content.Movie.Video.id,
+        url: content.Movie.Video.url,
+        duration: content.Movie.Video.duration,
+        sizeInKb: content.Movie.Video.sizeInKb,
+        createdAt: new Date(content.Movie.Video.createdAt),
+        updatedAt: new Date(content.Movie.Video.updatedAt),
+      });
+
+      contentEntity.addMedia(
+        MovieEntity.createFrom({
+          id: content.Movie.id,
+          video,
+          createdAt: new Date(content.Movie.createdAt),
+          updatedAt: new Date(content.Movie.updatedAt),
+        }),
+      );
+
+      if (content.Movie.Thumbnail) {
+        const thumbnail = ThumbnailEntity.createFrom({
+          id: content.Movie.Thumbnail.id,
+          url: content.Movie.Thumbnail.url,
+          createdAt: new Date(content.Movie.Thumbnail.createdAt),
+          updatedAt: new Date(content.Movie.Thumbnail.updatedAt),
+        });
+
+        contentEntity.getMedia()?.addThumbnail(thumbnail);
+      }
+    }
+
+    return contentEntity;
+  }
+
+  private isMovie(content: unknown): content is Prisma.ContentGetPayload<{
+    include: {
+      Movie: {
+        include: { Video: true };
+      };
+    };
+  }> {
+    if (typeof content === 'object' && content !== null && 'Movie' in content) {
+      return true;
+    }
+
+    return false;
   }
 
   private extractErrorMessage(error: unknown): string {
