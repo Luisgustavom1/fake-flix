9bbc5658a0f01666ceff6ab8429dd221494c53f5
Luis Gustavo <luisgustavomacedo13@gmail.com>
2025-06-21 16:15:45 -0300

    containering app

--- src/module/content/content.module.ts
+++ src/module/content/content.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ContentAdminModule } from './admin/content-admin.module';
-import { ContentVideoProcessorModule } from './video-processor/content-video-processor.module';
 import { ContentCatalogModule } from './catalog/content-catalog.module';
 
 @Module({
-  imports: [
-    ContentAdminModule,
-    ContentVideoProcessorModule,
-    ContentCatalogModule,
-  ],
+  imports: [ContentAdminModule, ContentCatalogModule],
 })
 export class ContentModule {}
